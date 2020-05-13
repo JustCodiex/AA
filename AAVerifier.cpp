50a61a745d590d6c22a2c682a930e4d8beb6bd30
JustCoDiEx <codiex1944@gmail.com>
2020-05-13 23:41:03 +0200

    Added files for internal verifier.

---
+++ AAVerifier.cpp
@@ -0,0 +1,7 @@
+#include "AAVerifier.h"
+
+bool AAVerifier::VerifyStack(aa::list<uint16_t> operations) {
+
+	return true;
+
+}
---
+++ AAVerifier.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "list.h"
+
+
+class AAVerifier {
+
+public:
+
+	bool VerifyStack(aa::list<uint16_t> operations);
+
+private:
+
+
+
+};
